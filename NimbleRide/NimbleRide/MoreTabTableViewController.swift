0c803f8a1635a9b7063dcdd510004074d07eb14a
Alejandro Puente <alejandro@glooko.com>
2017-04-23 15:18:54 -0700

    new tableView for more screen

---
+++ NimbleRide/NimbleRide/MoreTabTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MoreTabTableViewController.swift
+//  NimbleRide
+//
+//  Created by Alejandro Puente on 4/23/17.
+//  Copyright © 2017 Alejandro Puente. All rights reserved.
+//
+
+import Foundation
