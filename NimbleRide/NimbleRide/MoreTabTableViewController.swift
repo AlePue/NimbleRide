50394db88838573140796a047e90deaa955e6db8
Alejandro Puente <alejandro@glooko.com>
2017-04-23 15:19:01 -0700

    new tableView for more screen

--- NimbleRide/NimbleRide/MoreTabTableViewController.swift
+++ NimbleRide/NimbleRide/MoreTabTableViewController.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2017 Alejandro Puente. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+import FBSDKCoreKit
+
+class MoreTabTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let list = ["Profile", "Settings", "Help", "Pair to E-Bike"]
+    var myIndex = 0
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return(list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MoreTabViewControllerViewCell
+        cell.myLabel.text = list[indexPath.row]
+        
+        return (cell)
+
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        myIndex = indexPath.row
+        debugPrint("\(myIndex)")
+        if(myIndex == 0){
+        performSegue(withIdentifier: "Profile", sender: self)
+        } else if (myIndex == 1) {
+        performSegue(withIdentifier: "Settings", sender: self)
+        } else if (myIndex == 2) {
+        performSegue(withIdentifier: "Help", sender: self)
+        } else if (myIndex == 3) {
+        performSegue(withIdentifier: "Pair", sender: self)
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- NimbleRide/NimbleRide/MoreTabTableViewControllerViewCell.swift
+++ NimbleRide/NimbleRide/MoreTabTableViewControllerViewCell.swift
@@ -1,76 +1,28 @@
 //
-//  MoreTabViewController.swift
+//  MoreTabViewControllerViewCell.swift
 //  NimbleRide
 //
 //  Created by Alejandro Puente on 4/2/17.
 //  Copyright © 2017 Alejandro Puente. All rights reserved.
 //
 
-//import UIKit
-//import FBSDKCoreKit
-//
-//class MoreTabViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-//    
-//    
-//    let list = ["e", "em"]
-//    
-//    
-//    @available(iOS 2.0, *)
-//    
-//     func tableView(_ tableView: UItableView, numberOfRowsInSection section: Int) -> Int {
-//        return(list.count)
-//    }
-//    
-//     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableView {
-//        let cell = UITableViewCell(style: <#T##UITableViewCellStyle#>.default, reuseIdentifier: "cell")
-//        cell.textLabel?.text = list[IndexPath.row]
-//    }
-//    
-//        override func viewDidLoad() {
-//            super.viewDidLoad()
-//            // Do any additional setup after loading the view, typically from a nib.
-//        }
-//        
-//        override func didReceiveMemoryWarning() {
-//            super.didReceiveMemoryWarning()
-//            // Dispose of any resources that can be recreated.
-//        }
-//        
-//        
-//}
-//    
-
 
 import UIKit
-import FBSDKCoreKit
 
-class MoreTabViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MoreTabViewControllerViewCell: UITableViewCell {
     
-    let list = ["Milk", "Honey", "Bread", "Tacos", "Tomatoes"]
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        return(list.count)
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
     }
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = list[indexPath.row]
+    override func setSelected(_ sleected: Bool, animated: Bool) {
+        super.setSelected(isSelected, animated: animated)
         
-        return(cell)
+        
     }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
 }
+
