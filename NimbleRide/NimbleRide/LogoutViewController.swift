7731ffc307748b6507e67ddd57e5734e5e0a9b35
Alejandro Puente <alejandro@glooko.com>
2017-05-08 18:33:22 -0700

    addded logout capabilities

--- NimbleRide/NimbleRide/LogoutViewController.swift
+++ NimbleRide/NimbleRide/LogoutViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class LogoutViewController: UIViewController {
 
+  
+    @IBAction func logoutButton(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
