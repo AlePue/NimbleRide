9722659ab53bfbac878b996bdc082e0fd3ddfe65
Eric Lau <laueric92@gmail.com>
2017-05-09 16:16:39 -0700

    VC button implemented, needs to change color

--- NimbleRide/NimbleRide/RideViewController.swift
+++ NimbleRide/NimbleRide/RideViewController.swift
@@ -542,31 +542,6 @@ class MapRideViewController: UIViewController, CLLocationManagerDelegate, MKMapV
     
 }
 
-//extension UIImage {
-//    
-//    func maskWithColor( color:UIColor) -> UIImage {
-//        
-//        UIGraphicsBeginImageContextWithOptions(self.size, false, UIScreen.main.scale)
-//        let context = UIGraphicsGetCurrentContext()!
-//        
-//        color.setFill()
-//        
-//        context.translateBy(x: 0, y: self.size.height)
-//        context.scaleBy(x: 1.0, y: -1.0)
-//        
-//        let rect = CGRect(x: 0.0, y: 0.0, width: self.size.width, height: self.size.height)
-//        context.draw(self.cgImage!, in: rect)
-//        
-//        context.setBlendMode(CGBlendMode.sourceIn)
-//        context.addRect(rect)
-//        context.drawPath(using: CGPathDrawingMode.fill)
-//        
-//        let coloredImage = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//        
-//        return coloredImage!
-//    }
-//}
 extension UIImage {
     func maskWithColor(color: UIColor) -> UIImage {
         
